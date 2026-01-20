642317edad85a5b1fa5c68f5ad3de87ad35a7795
catbrandt <16284@coderacademy.edu.au>
2026-01-20 13:15:20 +1100

    feat: booking models file drafted

--- src/models/Booking.js
+++ src/models/Booking.js
@@ -1,4 +1,5 @@
-const db = require('../config/db')
+const e = require('cors')
+const db = require('../db/pool.js')
 
 const createBooking = async (eventId, spaceId, UserId, quantity, totalPrice, paymentStatus) => {
   const result = await db.query(
@@ -8,4 +9,76 @@ const createBooking = async (eventId, spaceId, UserId, quantity, totalPrice, pay
     [eventId, spaceId, userId, quantity, totalPrice, paymentStatus],
   )
   return result.rows[0]
+}
+const getAllBookings = async () => {
+    const result = await db.uery('SELECT * FROM bookings ORDER BY id')
+    return result.rows
+}
+
+const getBookingById = async (id) => {
+    const result = await db.query('SELECT * FROM bookings WHERE id = $1', [id])
+}
+
+const getBookingsByUserId = async (userId) => {
+  const result = await db.query(
+    `SELECT 
+        b.*, 
+        e.title as event_title, 
+        e.start_at as event_start, 
+        e.end_at as event_end,
+        s.name as space_name,
+        s.address as space_address,
+        s.city as space_city
+        FROM bookings b 
+        LEFT JOIN events e ON b.event_id = e.id 
+        LEFT JOIN spaces s ON b.space_id = s.id
+        WHERE b.user_id = $1 
+        ORDER BY COALESCE(e.start_at, b.id) DESC`,
+        [userId],
+  )
+  return result.rows
+}
+
+const getBookingsByEventId = async (eventId) => {
+    const result = await db.query(
+        `SELECT b.*, u.full_name, u.email
+        FROM bookings b
+        JOIN users u ON b.user_id = u.id
+        WHERE b.space_id = $1
+        ORDER BY b.id`,
+        [spaceId]
+    )
+    return result.rows
+}
+
+const updateBooking = async (id, quantity, totalPrice, paymentStatus) => {
+    const result = await db.query(
+        `UPDATE bookings
+        SET quantity = $1, total_price = $2, payment_status = $3
+        WHERE id = $4
+        RETURNING *`,
+        [quantity, totalPrice, paymentStatus, id]
+    )
+    return result.rows[0]
+}
+
+const deleteBooking = async (id) => {
+    const result = await db.query(
+        `DELETE FROM bookings
+        WHERE id = $1
+        RETURNING *`,
+        [id]
+    )
+    return result.rows[0]
+}
+
+module.exports = {
+    createBooking,
+    getAllBookings,
+    getBookingById,
+    getBookingsByUserId,
+    getBookingsByEventId,
+    getBookingsBySpaceId,
+    updateBooking,
+    deleteBooking,
 }
